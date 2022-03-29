48b4d8a95ca5665635a18dd7faa185dfccda4f92
Shubham <ssp671995@gmail.com>
2022-03-29 23:55:23 +0530

    Added the welcome message.

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -3,6 +3,6 @@ package com.bridgelabz;
 public class BinarySearchTree {
 
     public static void main(String[] args) {
-        System.out.println("WELCOME TO BINARY-SEARCH-PROGRAM");
+        System.out.println("Welcome to the binary search tree.");
     }
 }
