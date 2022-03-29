70c131c1eabeac761bb16e6465cd6e104f613aee
Shubham <ssp671995@gmail.com>
2022-03-30 00:03:56 +0530

    Added the search function in node binary search tree.

--- src/com/bridgelabz/NodeBinarySearchTree.java
+++ src/com/bridgelabz/NodeBinarySearchTree.java
@@ -5,24 +5,41 @@ class Node {
     Node left, right;
 }
 public class NodeBinarySearchTree {
-    public Node insert(Node node, int data) {
-        if (node == null) {
+    public Node insert(Node node,int data) {
+        if (node == null ){
             return createNode(data);
         }
-        if (data < node.data) {
-            node.left = insert(node.left, data);
+        if ( data < node.data){
+            node.left = insert(node.left,data);
 
-        } else if (data > node.data) {
-            node.right = insert(node.right, data);
+        }else if (data > node.data){
+            node.right = insert(node.right,data);
         }
         return node;
     }
-
     private Node createNode(int data) {
         Node root = new Node();
         root.data = data;
         root.left = null;
         root.right = null;
         return root;
+    }
+
+    public boolean searchNode(Node node,int data){
+        if (node == null) return false;
+        boolean isPresent = false;
+        while (node != null){
+            if (data < node.data){
+                node = node.left;
+            }
+            else if(data > node.data){
+                node=node.right;
+            }
+            else {
+                isPresent = true;
+                break;
+            }
+        }
+        return  isPresent;
     }
 }
